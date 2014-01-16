a8bb25e0d01cc935d8fdacba338c3b8698f47b50
jlln <james.allen.1248@gmail.com>
2014-01-16 14:18:59 +0800

    libraries

--- CorrelationPlotMatrix.R
+++ CorrelationPlotMatrix.R
@@ -19,6 +19,8 @@ cols<-sort(cols)
 
 
 
+
+#Remove the redundant cells on the upper-right side.
 for (i in 2:length(cols)){
   column=cor.matrix[,cols[i]]
   print(column)
@@ -27,21 +29,18 @@ for (i in 2:length(cols)){
   }}
 
 
-
-
-
 cor.matrix
 cor.melt<-melt(cor.matrix)
 cor.melt
-
 
 cor.melt <- cor.melt[-which(is.na(cor.melt[, 3])),]
 
 cor.melt
 dat.cor<-data.frame(cor.melt)
 
-
 dat.cor$X1 = with(dat.cor, factor(X1, levels = rev(levels(X1))))
 
+
+#Print the graph.
 qplot(x=X2,y=X1,data=dat.cor,fill=value,geom='tile')+theme(legend.position = c(0.9, 0.7),legend.justification = c(1, 0),legend.direction = "horizontal",panel.background=element_blank(),axis.text.x=element_text(angle=-90))+scale_fill_gradientn(name="Correlation Coefficient",colours = myPalette(100))+geom_text(aes(X2, X1, label = value), color = "#073642", size = 4)+guides(fill = guide_colorbar(barwidth = 7, barheight = 1, title.position = "top",
                                                                                                                                                                                                                                                                                                                                                                                                             title.hjust = 0.5)) +labs(x="",y="")
