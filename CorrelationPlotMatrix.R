4a234dada48e282cec09b35521f74f4249d13de5
jlln <james.allen.1248@gmail.com>
2015-05-01 10:43:18 +0800

    Update CorrelationPlotMatrix.R
    Fixes to make text more readable

--- CorrelationPlotMatrix.R
+++ CorrelationPlotMatrix.R
@@ -1,7 +1,7 @@
 corMat<-function(dat){
   require(ggplot2)
   require(RColorBrewer)
-  require(reshape2)
+  require(reshape)
   
   
   #Establish the palette to be used
@@ -9,8 +9,6 @@ corMat<-function(dat){
   
   
   #Calculate the correlation between variables. If desired, alternative measures of correlation eg. Spearman's can be substituted.
-  dat<-dat[,order(colnames(tt))]
-  
   dat.cor=(round(cor(dat),digits=2))
   cor.matrix<-dat.cor
   
@@ -41,11 +39,11 @@ corMat<-function(dat){
   cor.melt
   dat.cor<-data.frame(cor.melt)
   
-  dat.cor$Var1 = with(dat.cor, factor(Var1, levels = rev(levels(Var1))))
+  dat.cor$X1 = with(dat.cor, factor(X1, levels = rev(levels(X1))))
   
   
   #Print the graph.
-  p<-qplot(x=Var2,y=Var1,data=dat.cor,fill=value,geom='tile')+theme(legend.position = c(0.9, 0.7),legend.justification = c(1, 0),legend.direction = "horizontal",panel.background=element_blank(),axis.text.x=element_text(angle=-90))+scale_fill_gradientn(name="Correlation Coefficient",colours = myPalette(100))+guides(fill = guide_colorbar(barwidth = 7, barheight = 1, title.position = "top",
+  p<-qplot(x=X2,y=X1,data=dat.cor,fill=value,geom='tile')+theme(legend.position = c(0.9, 0.7),legend.justification = c(1, 0),legend.direction = "horizontal",panel.background=element_blank(),axis.text.y=element_text(color="black",size=12),axis.text.x=element_text(,size=12,angle=45,color="black",hjust=1))+scale_fill_gradientn(name="Correlation Coefficient",colours = myPalette(100))+geom_text(aes(X2, X1, label = value), color = "#073642", size = 4)+guides(fill = guide_colorbar(barwidth = 7, barheight = 1, title.position = "top",
                                                                                                                                                                                                                                                                                                                                                                                                                  title.hjust = 0.5)) +labs(x="",y="")
   print(p)
   return(p)
